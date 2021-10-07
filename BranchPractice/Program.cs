4900b4672e62b3f0344d128c6d404fd0337edf36
Yousif <ybeeai@gmail.com>
2021-10-07 13:13:43 -0400

    first commit

---
+++ BranchPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BranchPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static void Number1(int num1,int num2)
+        {
+
+        }
+
+        static void Number2(int num1, int num2)
+        {
+
+        }
+
+    }
+}
