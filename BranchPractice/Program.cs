b716edfe6010c3d49a02ee8507dbb291b8d1ed41
Yousif <ybeeai@gmail.com>
2021-10-07 13:28:24 -0400

    first method

--- BranchPractice/Program.cs
+++ BranchPractice/Program.cs
@@ -6,11 +6,17 @@ namespace BranchPractice
     {
         static void Main(string[] args)
         {
-            
+            Number1(4, 3);
         }
-        static void Number1(int num1,int num2)
+        static bool Number1(int num1, int num2)
         {
+            if (num1 > num2)
+            {
+                Console.WriteLine($"{num1} is larger than {num2}");
+                return true;
 
+            }
+            return false;
         }
 
         static void Number2(int num1, int num2)
